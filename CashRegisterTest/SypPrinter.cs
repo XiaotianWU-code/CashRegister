9c12669d18decc449cc51f00ff085801b3c67171
Xiaotian WU <XWu32@slb.com>
2022-10-26 15:16:52 +0800

    test: verify print be called

---
+++ CashRegisterTest/SypPrinter.cs
@@ -0,0 +1,19 @@
+﻿using CashRegister;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegisterTest
+{
+    public class SypPrinter : Printer
+    {
+        public bool HasPrintedV1 { get; set; }
+
+        public override void Print(string content)
+        {
+            this.HasPrintedV1 = true;
+        }
+    }
+}
