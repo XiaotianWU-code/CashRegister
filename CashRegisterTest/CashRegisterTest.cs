91479d7d2b75933547c7f216c1ad0e868b497977
Xiaotian WU <XWu32@slb.com>
2022-10-26 16:21:03 +0800

    test: use mock for test

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,6 +1,8 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
+	using Moq;
+	using System.Reflection;
 	using Xunit;
 
 	public class CashRegisterTest
@@ -18,5 +20,34 @@ namespace CashRegisterTest
 			//verify that cashRegister.process will trigger print
 			Assert.True(printer.HasPrintedV1);
 		}
-	}
+
+		[Fact]
+		public void Should_call_print_when_run_process_given_spy_printer()
+		{
+			//Given
+			var printer = new Mock<Printer>();
+			var cashRegister = new CashRegister(printer.Object);
+			var purchase = new Purchase();
+			// When
+			cashRegister.Process(purchase);
+			// Then
+			printer.Verify(_ => _.Print(It.IsAny<string>()));
+        }
+
+		[Fact]
+		public void Should_print_purchase_content_when_run_process_given_stub_printer()
+		{
+			// Given
+			var sypPrinter = new Mock<Printer>();
+            var cashRegister = new CashRegister(sypPrinter.Object);
+            var stubPurchase = new Mock<Purchase>();
+			stubPurchase.Setup(_ => _.AsString()).Returns("mock content");
+
+			// When
+			cashRegister.Process(stubPurchase.Object);
+
+			// Then
+			sypPrinter.Verify(_ => _.Print("mock content"));
+        }
+    }
 }
